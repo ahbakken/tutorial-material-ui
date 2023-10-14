0d6c35037c654303b6277a8efd2f931cc3a09fe2
Astrid Bakken <astridbakken95@gmail.com>
2023-10-14 19:10:32 +0200

    feat: initial commit

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+
+export default function Home() {
+  return (
+    <>
+      <Button variant="outlined" onCklick>
+        Click me
+      </Button>
+    </>
+  );
+}
